bbbd6779f3a9492485079b33d2f3e2acdabb2781
Fanirintsoa Mathieu Andrianandraina <fanirintsoaandrianandraina@gmail.com>
2022-05-25 20:38:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 70){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+
+    });
+
+
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Apprenti dev", "Passioné d'art visuel"],
+        typeSpeed: 150,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: [ "Apprenti developpeur", "Passioné d'art visuel"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    
+   
+});
